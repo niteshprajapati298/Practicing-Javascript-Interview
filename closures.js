f84982ca9a23c15722e451a34014880b19dc4b65
Nitesh Prajapati <niteshkmrprajapati298@gmail.com>
2025-09-07 16:04:30 +0530

    revising-> closurs and optional chaining

---
+++ closures.js
@@ -0,0 +1,42 @@
+const user = {
+    firstName : "Nitesh",
+    lastName : "Prajapati",
+    age:20,
+}
+// optional chaining
+
+// console.log(user.adress);// It is will execute and It will give undefined
+// console.log(user.adress.city);// It will give error bcoz we are checking directly in the user adress and in the city cannot read properties of undefined city
+// console.log(user.adress?.city); // It will not give error it will give undefined bcoz there is no address in user object
+// --> execution flow -> the code will check if user ke andar adress exists karta hai to city ko check karo warna mat kro that is optional chaining prevent from errors
+
+const arr = [];
+arr.push(1);
+arr.push("2");
+const arr2 = arr.forEach(el => {
+    el = el+2;
+});
+console.log(arr2);
+
+
+// A closure is a function that remembers the variables from its outer scope even after the outer function has finished executing.
+
+function outer() {
+  let counter = 0; // parent scope variable
+
+  function inner() {
+    counter++;
+    console.log(counter);
+  }
+
+  return inner;
+}
+
+const fn = outer(); 
+fn(); // 1
+fn(); // 2
+fn(); // 3
+
+let ans = parent();
+console.log(ans);
+
